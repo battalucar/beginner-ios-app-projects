4d08891b9c3ac6efb7791e2bde70ad6470784be7
Battal Ucar <battalucar@outlook.com>
2022-03-14 07:37:27 +0300

    first-commit

---
+++ 02.i-am-poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Battal Uçar on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 03.dice-roller/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    private let diceArray: [UIImage] = [
+        UIImage(named: "DiceOne")!,
+        UIImage(named: "DiceTwo")!,
+        UIImage(named: "DiceThree")!,
+        UIImage(named: "DiceFour")!,
+        UIImage(named: "DiceFive")!,
+        UIImage(named: "DiceSix")!
+    ]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }
+    
+    
+}
+
